dba341f328ac523b12d4f1bf48871b78476c3ba7
Noel Mbuya <mbuyanoel@gmail.com>
2022-05-07 20:22:57 +0300

    Add project structure / breakdown

---
+++ aes_impl.py
@@ -0,0 +1,17 @@
+# This is the version without a third party library implementation.
+
+#  Tasks
+# Create an expansion key
+# Index iterator function for the exp_key
+# Lookup table / Matrix and associated functions
+# Functions for sub, rotate and rc
+# 
+# Key Generation
+# Encrypting function
+# 
+# Decrypting function
+# 
+
+
+
+
